d53369f236e600df0378ab2eb39afda1e361fd04
Kazuki Okamoto <kazuki.okamoto@kakkun61.com>
2015-06-04 22:54:14 +0900

    <a /> のテスト通過

--- src/Language/Snx.hs
+++ src/Language/Snx.hs
@@ -29,7 +29,7 @@ decode = decodeElem 0 . lines
 
 decodeElem :: Int -> [Snx] -> Xml
 decodeElem nest snxs@(snx : rest)
-  | ind snx /= 2 * nest                  = fail "illegal indent"
+  | ind snx /= indent * nest                  = fail "illegal indent"
   | ':' == head (dropWhile (== ' ') snx) = decodeTextElem nest snxs
   | otherwise                            = decodeTagElem nest snxs
 decodeElem _ [] = undefined
@@ -43,24 +43,23 @@ decodeTextElem _ [] = undefined
 
 decodeTagElem :: Int -> [Snx] -> Xml
 decodeTagElem nest snxs@(snx : rest) =
-  if length rest == 0
-  then
-    trace ("closing tag: " ++ tag) undefined
-  else
-    decodeInTagElem tag nest rest
+  replicate (indent * nest) ' ' ++ "<" ++ tag ++ decodeInTagElem tag nest rest
   where
-    tag = snx
+    tag = dropWhile (== ' ') snx
 decodeTagElem _ [] = undefined
 
 decodeInTagElem :: String -> Int -> [Snx] -> Xml
 decodeInTagElem tag nest snxs@(snx : rest) =
   case ind snx of
-    i | i == 2 * (nest + 2) -> trace "attr" undefined
-      | i == 2 * (nest + 1) -> trace "sub elem" undefined
-      | i == 2 * nest       -> trace "next elem" undefined
+    i | i == indent * (nest + 2) -> trace "attr" undefined
+      | i == indent * (nest + 1) -> trace "sub elem" undefined
+      | i == indent * nest       -> trace "next elem" undefined
     otherwise               -> fail "illegal indent"
-decodeInTagElem _ _ [] = undefined
+decodeInTagElem tag _ [] = " />\n"
 
 -- | count leading spaces
 ind :: String -> Int
 ind = length . takeWhile (== ' ')
+
+indent :: Int
+indent = 2
