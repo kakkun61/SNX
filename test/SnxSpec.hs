ff064579f8d2c90c2ede1a249522a8565590e404
Kazuki Okamoto <kazuki.okamoto@kakkun61.com>
2015-06-05 22:52:49 +0900

    入れ子になったタグ

--- test/SnxSpec.hs
+++ test/SnxSpec.hs
@@ -5,8 +5,15 @@ import Language.Snx
 
 spec :: Spec
 spec = do
-  it "single tag element" $ do
-    decode "a" `shouldBe` "<a />\n"
+  describe "normal" $ do
+    it "single tag element" $ do
+      decode "a" `shouldBe` "<a />\n"
 
-  it "single tag element with an attribute" $ do
-    decode "a\n    p\n" `shouldBe` "<a\n    p />\n"
+    it "single tag element with an attribute" $ do
+      decode "a\n    p\n" `shouldBe` "<a\n    p />\n"
+
+    it "1 child tag elem" $ do
+      decode "a\n  b\n" `shouldBe` "<a>\n  <b />\n</a>\n"
+
+    it "2-nested tag elem" $ do
+      decode "a\n  b\n    c\n" `shouldBe` "<a>\n  <b>\n    <c />\n  </b>\n</a>\n"
