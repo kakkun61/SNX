891ba79e36f7f448acacd3378b97193fb623d14c
Kazuki Okamoto <kazuki.okamoto@kakkun61.com>
2015-06-08 19:58:34 +0900

    デバッグ出力とエラーが出なくなったが間違った実装
    <a
        p>
      <b
          q
          r>
        <c
            s />
        <d />
      </b>
      <e />
    </a>
    を期待するが
    <a
        p>
      <b
          q
          r>
        <c
            s />
        <d />
      <e /> ← NG
      </b> ← NG
    </a>
    になる

--- src/Language/Snx.hs
+++ src/Language/Snx.hs
@@ -4,6 +4,7 @@ import Debug.Trace
 
 type Xml = String
 type Snx = String
+type LineNum = Int
 
 -- |
 -- 下記コードから
@@ -25,47 +26,48 @@ type Snx = String
 -- >>> decode "a\n  |b\n    c\n  d"
 -- "<a><b><c /></b><d />< a>"
 decode :: Snx -> Xml
-decode = decodeElem 0 . lines
+decode = decodeElem 0 1 . lines
 
-decodeElem :: Int -> [Snx] -> Xml
-decodeElem nest snxs@(snx : rest)
-  | countIndent snx /= nest   = error "illegal indent"
-  | ':' == head (unshift snx) = decodeTextElem nest snxs
-  | otherwise                 = decodeTagElem nest snxs
-decodeElem _ [] = undefined
+decodeElem :: Int -> LineNum -> [Snx] -> Xml
+decodeElem nest ln snxs@(snx : rest)
+  | trace ("decodeElem " ++ (show nest) ++ " " ++ (show ln) ++ " \"" ++ snx ++ "...\"") False = error "never come here"
+  | countIndent ln snx /= nest = syntaxError "illegal indent (decodeElem)" ln snx
+  | ':' == head (unshift snx)  = decodeTextElem nest ln snxs
+  | otherwise                  = decodeTagElem nest ln snxs
+decodeElem _ _ [] = ""
 
-decodeTextElem :: Int -> [Snx] -> Xml
-decodeTextElem nest snxs@(snx : rest) =
+decodeTextElem :: Int -> LineNum -> [Snx] -> Xml
+decodeTextElem nest ln snxs@(snx : rest) =
   trace ("text elem: " ++ text) undefined
   where
     text = drop 2 snx
-decodeTextElem _ [] = undefined
+decodeTextElem _ _ [] = undefined
 
-decodeTagElem :: Int -> [Snx] -> Xml
-decodeTagElem nest snxs@(snx : rest) =
-  shift nest $ "<" ++ tag ++ decodeInTagElem tag nest rest
+decodeTagElem :: Int -> LineNum -> [Snx] -> Xml
+decodeTagElem nest ln snxs@(snx : rest) = trace ("decodeTagElem " ++ (show nest) ++ " " ++ (show ln) ++ " \"" ++ snx ++ "...\"") $
+  shift nest $ "<" ++ tag ++ decodeInTagElem tag nest (ln + 1) rest
   where
     tag = unshift snx
-decodeTagElem _ [] = undefined
+decodeTagElem _ _ [] = undefined
 
-decodeInTagElem :: String -> Int -> [Snx] -> Xml
-decodeInTagElem tag nest snxs@(snx : rest) =
-  case countIndent snx of
-    i | i == nest + 2 -> "\n" ++ snx ++ decodeInTagElem tag nest rest
-      | i == nest + 1 -> ">\n" ++ decodeElem (nest + 1) snxs ++ shift nest ("</" ++ tag ++ ">\n")
-      | i == nest     -> " />\n" ++ decodeElem nest snxs
-    _                 -> error "illegal indent"
-decodeInTagElem _ _ [] = " />\n"
+decodeInTagElem :: String -> Int -> LineNum -> [Snx] -> Xml
+decodeInTagElem tag nest ln snxs@(snx : rest) = trace ("decodeInTagElem " ++ tag ++ " " ++ (show nest) ++ " " ++ (show ln) ++ " \"" ++ snx ++ "...\"") $
+  case countIndent ln snx of
+    i | i == nest + 2 -> "\n" ++ snx ++ decodeInTagElem tag nest (ln + 1) rest
+      | i == nest + 1 -> ">\n" ++ decodeElem (nest + 1) ln snxs ++ shift nest ("</" ++ tag ++ ">\n")
+      | i <= nest     -> " />\n" ++ decodeElem i ln snxs
+      | otherwise     -> syntaxError ("illegal indent (actual " ++ (show i) ++ ", expexted " ++ (show nest) ++ " or " ++ (show (nest + 1)) ++ " or " ++ (show (nest + 2)) ++ ")") ln snx
+decodeInTagElem _ _ _ [] = " />\n"
 
 -- | count leading spaces
-countIndent :: String -> Int
-countIndent text =
+countIndent :: LineNum -> String -> Int
+countIndent ln text =
   let
     n = length (takeWhile (== ' ') text)
   in
     if n `mod` indent == 0
     then n `div` indent
-    else error "illegal indent"
+    else syntaxError ("illegal indent (not any multiples of " ++ (show indent)) ln text
 
 indent :: Int
 indent = 2
@@ -75,3 +77,6 @@ shift n text = replicate (n * indent) ' ' ++ text
 
 unshift :: String -> String
 unshift = dropWhile (== ' ')
+
+syntaxError :: String -> LineNum -> Snx -> a
+syntaxError msg ln snx = error $ msg ++ " at " ++ (show ln) ++ " (" ++ snx ++ ")"
